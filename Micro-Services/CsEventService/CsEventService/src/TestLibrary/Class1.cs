89ef2d559faf1af48ca79eb832c744b7eb404718
jlenormand <jlenormand@NANTES-0158>
2017-02-21 10:58:42 +0100

    Unit Tests now working

---
+++ Micro-Services/CsEventService/CsEventService/src/TestLibrary/Class1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+        public void testTrue()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
