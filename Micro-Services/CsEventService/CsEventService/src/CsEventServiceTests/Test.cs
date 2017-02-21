228b44c7910ea80d2c3c1e84e6ea5896e34e232e
jlenormand <jlenormand@NANTES-0158>
2017-02-21 10:58:42 +0100

    Unit Tests now working

---
+++ Micro-Services/CsEventService/CsEventService/src/CsEventServiceTests/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CsEventServiceTests
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void testTrue()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
