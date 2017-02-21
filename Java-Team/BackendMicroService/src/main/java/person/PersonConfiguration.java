745d59e7fd5ec13fd55ce652fe6606e7a82771a8
Pierre GAULTIER <pgaultier@sii.fr>
2017-02-21 14:48:50 +0100

    first backend, need to refactor later

---
+++ Java-Team/BackendMicroService/src/main/java/person/PersonConfiguration.java
@@ -0,0 +1,13 @@
+package person;
+
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan
+@EntityScan("person")
+public class PersonConfiguration {
+
+}
