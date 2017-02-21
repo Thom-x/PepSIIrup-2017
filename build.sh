1bfe9642bada4df57fcbdfea8cfa9504d94bfa0d
jlenormand <jlenormand@NANTES-0158>
2017-02-21 15:57:22 +0100

    travis-ci tests

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-dotnet restore && dotnet build **/project.json
+dotnet restore && dotnet build **/project.json && dotnet install NUnit.Runners -Version 2.6.4 -OutputDirectory testrunner
