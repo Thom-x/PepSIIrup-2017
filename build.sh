8eff8d3573e1b022d0adb89955b2df351f652b35
jlenormand <jlenormand@NANTES-0158>
2017-02-22 14:40:21 +0100

    pushing new config file

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-C:/SonarQube/bin/SonarQube.Scanner.MSBuild.exe begin /key:"org.sonarqube:sonarqube-scanner-msbuild" /name:"TraineeSIIp" /version:"1.0"
+SonarQube.Scanner.MSBuild.exe begin /key:"org.sonarqube:sonarqube-scanner-msbuild" /name:"TraineeSIIp" /version:"1.0"
 dotnet restore && dotnet build **/project.json && dotnet test Micro-Services/CsEventService/src/TestLibrary/project.json
-C:/SonarQube/bin/SonarQube.Scanner.MSBuild.exe end
+SonarQube.Scanner.MSBuild.exe end
