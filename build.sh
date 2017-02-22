70d13722c99aaba77e4e7bd0a6e2a1932433b038
jlenormand <jlenormand@NANTES-0158>
2017-02-22 15:05:01 +0100

    adding ./ in the sh file to run the exe

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-MSBuild.SonarQube.Runner.exe begin /key:"org.sonarqube:sonarqube-scanner-msbuild" /name:"TraineeSIIp" /version:"1.0"
+./MSBuild.SonarQube.Runner.exe begin /key:"org.sonarqube:sonarqube-scanner-msbuild" /name:"TraineeSIIp" /version:"1.0"
 dotnet restore && dotnet build **/project.json && dotnet test Micro-Services/CsEventService/src/TestLibrary/project.json
-MSBuild.SonarQube.Runner.exe end
+./MSBuild.SonarQube.Runner.exe end
