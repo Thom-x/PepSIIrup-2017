901213e198fa790b6d0fb6f6e2792ea05164fe61
Pierre GAULTIER <pgaultier@sii.fr>
2017-02-22 14:20:03 +0100

    karma jasmine test

--- Java-Team/Mobile-Ionic/www/js/controllers.js
+++ Java-Team/Mobile-Ionic/www/js/controllers.js
@@ -1,5 +1,5 @@
 angular.module('app.controllers', [])
-  
+
 .controller('accueilCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -7,7 +7,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('rechercheCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -15,44 +15,48 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('carteCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
 	var options = {timeout: 10000, enableHighAccuracy: true};
-	 
+
 	var latLng = new google.maps.LatLng(47.2112216, -1.5570168);
-	 
+
 	var mapOptions = {
 			center: latLng,
 			zoom: 15,
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
 	$scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
-	
+
 	var marker = new google.maps.Marker({position:new google.maps.LatLng(47.2112216, -1.5570168)});
 	marker.setMap($scope.map);
 	var infowindow = new google.maps.InfoWindow({
 		content: "<div style=\"display:inline-block\"><img src=\"img/logo.png\"style=\"display:inline;width:50px;height:50px;\"></div>" +
 				"<div style=\"display:inline-block\"><p>22 rue oui</p><p>***</p></div>"
 	});
-	
+
 	marker.addListener('click', function() {
 		infowindow.open($scope.map, marker);
 	  });
 
 }])
-   
+
 .controller('footEnSalleCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
+  //sum to test jasmine/karma
+  $scope.sum = function() {
+   $scope.z = $scope.x + $scope.y;
+  };
 
 }])
-   
+
 .controller('afterworkCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -60,7 +64,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('dejTechCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -68,7 +72,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -76,7 +80,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('mesVenementsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -84,7 +88,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('crErUnVenementCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -92,7 +96,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('suggestionCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -100,7 +104,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('evaluerEvenementCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -108,4 +112,3 @@ function ($scope, $stateParams) {
 
 
 }])
- 
