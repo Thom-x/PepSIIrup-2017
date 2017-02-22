0d160c8d68043e1d80690b836bb23f3acab2fcbc
jlenormand <jlenormand@NANTES-0158>
2017-02-22 11:58:29 +0100

    trying to get sonar working using sonnar-scanner

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+SonarQube.Scanner.MSBuild.exe begin /k:"org.sonarqube:sonarqube-scanner-msbuild" /n:"TraineeSIIp" /v:"1.0"
 dotnet restore && dotnet build **/project.json && dotnet test Micro-Services/CsEventService/src/TestLibrary/project.json
+SonarQube.Scanner.MSBuild.exe end
