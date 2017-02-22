d798988f0d698409edb8d6b6ac92ada985cd1281
jlenormand <jlenormand@NANTES-0158>
2017-02-22 14:13:16 +0100

    trying to add the runner directly on the root folder

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-"C:\SonarQube\bin\SonarQube.Scanner.MSBuild.exe" begin /k:"org.sonarqube:sonarqube-scanner-msbuild" /n:"TraineeSIIp" /v:"1.0"
+MSBuild.SonarQube.Runner.exe begin /k:"org.sonarqube:sonarqube-scanner-msbuild" /n:"TraineeSIIp" /v:"1.0"
 dotnet restore && dotnet build **/project.json && dotnet test Micro-Services/CsEventService/src/TestLibrary/project.json
-"C:\SonarQube\bin\SonarQube.Scanner.MSBuild.exe" end
+MSBuild.SonarQube.Runner.exe end
