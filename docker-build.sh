4c60c1d96266a72262cecbb5731bc0a5dd91f236
Pierre GAULTIER <pgaultier@sii.fr>
2017-03-28 15:29:25 +0200

    build ok and docker ok

---
+++ docker-build.sh
@@ -0,0 +1,9 @@
+docker stop $(docker ps -a -q)
+cd com.SII.RegisterService
+docker build -t eureka .
+cd ../com.SII.PersonneService
+docker build -t person .
+cd ../com.SII.EventService
+docker build -t event .
+cd ../com.SII.ClientService
+docker build -t client .
