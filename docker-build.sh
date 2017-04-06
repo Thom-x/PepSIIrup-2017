770376235396b0cf8bb9dc67df0b169352412fc3
Pierre GAULTIER <pgaultier@sii.fr>
2017-04-06 16:45:42 +0200

    tests

--- docker-build.sh
+++ docker-build.sh
@@ -8,7 +8,3 @@ cd ../com.SII.EventService
 docker build -t event .
 cd ../com.SII.ClientService
 docker build -t client .
-docker run -p 1111:1111 -d eureka
-docker run -p 2222:2222 -d person
-docker run -p 3333:3333 -d event
-docker run -p 80:80 -d client
